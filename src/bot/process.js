87dd08226f88d70f29f11d7ab9d8a85eb47ac194
artificial-life <proteus.protocol@gmail.com>
2016-06-07 15:52:57 +0600

    arm id in cmd args

--- src/bot/process.js
+++ src/bot/process.js
@@ -8,30 +8,34 @@ let boot = require('./boot.js')('process');
 let app = require('./app/app.js');
 
 if (process.argv[2] && process.argv[3]) {
-  Settings.setItem('login', process.argv[2]);
-  Settings.setItem('password', process.argv[3]);
+	Settings.setItem('login', process.argv[2]);
+	Settings.setItem('password', process.argv[3]);
+}
+
+if (process.argv[4]) {
+	Settings.setItem('control-panel_arm_id', process.argv[4]);
 }
 
 let iterations = 0;
 let interval_id = false;
 let current_ticket = false;
 
-let doFn = function(ws, interval, count) {
-  interval_id = setInterval(() => {
-    iterations++;
-    current_ticket && current_ticket.arrived().then(() => ws.getNext()).then(d => current_ticket = d);
-    if (iterations == count) clearInterval(interval_id);
-  }, interval);
+let doFn = function (ws, interval, count) {
+	interval_id = setInterval(() => {
+		iterations++;
+		current_ticket && current_ticket.arrived().then(() => ws.getNext()).then(d => current_ticket = d);
+		if (iterations == count) clearInterval(interval_id);
+	}, interval);
 }
 app.then((ws) => {
-  return new Promise((resolve) => {
-    ws.once('queue.update', (d) => resolve({
-      ticket: _.first(d.live.tickets),
-      ws: ws
-    }));
-  });
+	return new Promise((resolve) => {
+		ws.once('queue.update', (d) => resolve({
+			ticket: _.first(d.live.tickets),
+			ws: ws
+		}));
+	});
 }).then((d) => {
-  current_ticket = d.ticket;
-  console.log('Ticket', d.ticket.code);
-  doFn(d.ws, Settings.getItem('interval'), Settings.getItem('iterations_count'));
+	current_ticket = d.ticket;
+	console.log('Ticket', d.ticket.code);
+	doFn(d.ws, Settings.getItem('interval'), Settings.getItem('iterations_count'));
 });
